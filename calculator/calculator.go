f4126ee46a7031d5b2974c0a38aa386dfda98da5
Lucas Mihara <miharalucas@gmail.com>
2024-09-17 23:27:02 -0300

    starting advanced calculator

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -3,33 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	var n1, n2 float32
-	var operation rune
-
-	fmt.Println("Type the first number")
-	fmt.Scanf("%v\n", &n1)
-	fmt.Scanf("%v\n", &n2)
-	fmt.Println("Type the operation")
-	fmt.Scanf("%c", &operation)
-
-	var result float32
-	switch operation {
-		case '+':
-			result = n1 + n2
-		case '-':
-			result = n1 - n2
-		case '*':
-			result = n1 * n2
-		case '/':
-			if n2 == 0 {
-				fmt.Println("Division by zero")
-				return
-			}
-			result = n1 / n2
-		default:
-			fmt.Println("This operation is not supported")
-			return
-	}
-
-	fmt.Printf("Result: %v\n", result)
+	var expression string
+	fmt.Println("Type the expression")
+	fmt.Scanf("%s", &expression)
+	fmt.Printf("%s", expression)
 }
+// func main() {
+// 	var n1, n2 float32
+// 	var operation rune
+
+// 	fmt.Println("Type the first number")
+// 	fmt.Scanf("%v\n", &n1)
+// 	fmt.Scanf("%v\n", &n2)
+// 	fmt.Println("Type the operation")
+// 	fmt.Scanf("%c", &operation)
+
+// 	var result float32
+// 	switch operation {
+// 		case '+':
+// 			result = n1 + n2
+// 		case '-':
+// 			result = n1 - n2
+// 		case '*':
+// 			result = n1 * n2
+// 		case '/':
+// 			if n2 == 0 {
+// 				fmt.Println("Division by zero")
+// 				return
+// 			}
+// 			result = n1 / n2
+// 		default:
+// 			fmt.Println("This operation is not supported")
+// 			return
+// 	}
+
+// 	fmt.Printf("Result: %v\n", result)
+// }
