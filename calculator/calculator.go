5f83e46939bcb6020e64abf50ebe578afbb98f41
Lucas Mihara <miharalucas@gmail.com>
2024-09-25 21:36:55 -0300

    finished calculator

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 	"slices"
 	"strconv"
+	"errors"
 )
 
 const (
@@ -24,8 +25,97 @@ func main() {
 		return
 	}
 	
-	result := calculateSums(expression)
-	fmt.Println(result)
+	result, err := calculateSums(expression)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+}
+
+func calculateSums(expression string) (float64, error) {
+	parts, operations := breakParts(expression, []byte {Sum, Subtraction})
+	convertedValue, err := strconv.ParseFloat(parts[0], 64)
+	if err != nil {
+		convertedValue, err = calculateMultiplications(parts[0])
+		if err != nil {
+			return 0, err
+		}
+	}
+	result := convertedValue
+	for i := 0; i < len(operations); i++ {
+		convertedValue, err = strconv.ParseFloat(parts[i + 1], 64)
+		if err != nil {
+			convertedValue, err = calculateMultiplications(parts[i + 1])
+			if err != nil {
+				return 0, err
+			}
+		}
+		switch operations[i] {
+			case Sum:
+				result += convertedValue
+			case Subtraction:
+				result -= convertedValue
+		}
+	}
+	return result, nil
+}
+
+func calculateMultiplications(expression string) (float64, error) {
+	parts, operations := breakParts(expression, []byte {Multiplication, Division})
+	convertedValue, err := strconv.ParseFloat(parts[0], 64)
+	if err != nil {
+		convertedValue, err = resolveParentheses(parts[0])
+		if err != nil {
+			return 0, err
+		}
+	}
+	result := convertedValue
+	for i := 0; i < len(operations); i++ {
+		convertedValue, err = strconv.ParseFloat(parts[i + 1], 64)
+		if err != nil {
+			convertedValue, err = resolveParentheses(parts[i + 1])
+			if err != nil {
+				return 0, err
+			}
+		}
+		switch operations[i] {
+			case Multiplication:
+				result *= convertedValue
+			case Division:
+				if convertedValue == 0 {
+					return 0, errors.New("Division by zero")
+				}
+				result /= convertedValue
+		}
+	}
+	return result, nil
+}
+
+func resolveParentheses(expression string) (float64, error) {
+	return calculateSums(expression[1:(len(expression) - 1)])
+}
+
+func breakParts(expression string, operators []byte) ([]string, []byte) {
+	var parts []string
+	var operations []byte
+	parenthesesCount := 0
+	
+	start, end := 0, 0
+	for ; end < len(expression); end++ {
+		if expression[end] == '(' {
+			parenthesesCount++
+		} else if expression[end] == ')' {
+			parenthesesCount--
+		} else if parenthesesCount == 0 && (slices.Contains(operators, expression[end])) {
+			parts = append(parts, expression[start:end])
+			operations = append(operations, expression[end])
+			start = end + 1
+		}
+	}
+	parts = append(parts, expression[start:end])
+
+	return parts, operations
 }
 
 func isValid(expression string) bool {
@@ -51,91 +141,3 @@ func isValid(expression string) bool {
 
 	return true
 }
-
-func calculateSums(expression string) float64 {
-	parts, operations := breakParts(expression, []byte {Sum, Subtraction})
-	convertedValue, _ := strconv.ParseFloat(parts[0], 32)
-	result := convertedValue
-	for i := 0; i < len(operations); i++ {
-		convertedValue, _ = strconv.ParseFloat(parts[i + 1], 32)
-		switch operations[i] {
-			case Sum:
-				result += convertedValue
-			case Subtraction:
-				result -= convertedValue
-		}
-	}
-	// for i := 0; i < len(parts); i++ {
-	// 	fmt.Printf("%s ", parts[i])
-	// }
-	// for i := 0; i < len(operations); i++ {
-	// 	fmt.Printf("%c ", operations[i])
-	// }
-	return result
-}
-
-func breakParts(expression string, operators []byte) ([]string, []byte) {
-	var parts []string
-	var operations []byte
-	parenthesesCount := 0
-	
-	start, end := 0, 0
-	for ; end < len(expression); end++ {
-		if expression[end] == '(' {
-			parenthesesCount++
-		} else if expression[end] == ')' {
-			parenthesesCount--
-		} else if parenthesesCount == 0 && (slices.Contains(operators, expression[end])) {
-			parts = append(parts, expression[start:end])
-			operations = append(operations, expression[end])
-			start = end + 1
-		}
-	}
-	parts = append(parts, expression[start:end])
-
-	return parts, operations
-}
-
-// func calculateMultiplications(expression string) float32 {
-// 	var parts []string
-// 	var operations []Operation
-	
-// }
-
-// func resolveParentheses(expression string) float32 {
-// 	var parts []string
-// 	var operations []Operation
-	
-// }
-
-// func main() {
-// 	var n1, n2 float32
-// 	var operation rune
-
-// 	fmt.Println("Type the first number")
-// 	fmt.Scanf("%v\n", &n1)
-// 	fmt.Scanf("%v\n", &n2)
-// 	fmt.Println("Type the operation")
-// 	fmt.Scanf("%c", &operation)
-
-// 	var result float32
-// 	switch operation {
-// 		case '+':
-// 			result = n1 + n2
-// 		case '-':
-// 			result = n1 - n2
-// 		case '*':
-// 			result = n1 * n2
-// 		case '/':
-// 			if n2 == 0 {
-// 				fmt.Println("Division by zero")
-// 				return
-// 			}
-// 			result = n1 / n2
-// 		default:
-// 			fmt.Println("This operation is not supported")
-// 			return
-// 	}
-
-// 	fmt.Printf("Result: %v\n", result)
-// }
