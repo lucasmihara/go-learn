edc2e630da37fb3adaf392a63a0d71f8700239b7
Lucas Mihara <miharalucas@gmail.com>
2024-10-07 21:09:17 -0300

    start todolist

---
+++ todolist/todolist.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	// "os"
+)
+
+func main() {
+	fmt.Println("Choose a operation:")
+	fmt.Println("1 - [C]reate a new entry")
+	fmt.Println("2 - [R]emove a entry")
+
+	var option string
+	fmt.Scan(&option)
+
+	if option == "1" || option == "C" {
+
+	} else if option == "2" || option == "R" {
+		
+	}
+}
