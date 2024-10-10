0f23f75b2e9bb9e8cf11c94d01da18c703458754
Lucas Mihara <miharalucas@gmail.com>
2024-10-09 21:19:34 -0300

    finished todolistr

--- todolist/todolist.go
+++ todolist/todolist.go
@@ -9,34 +9,26 @@ import (
 
 func main() {
 	fileName := getFileName()
-	fileData, err := os.ReadFile(fileName)
+	tasks, err := getTasksFromFile(fileName)
 	if err != nil {
 		fmt.Println("Error on loading file")
 		fmt.Println(err)
 		return
 	}
-	fileContent := string(fileData)
-
-	entries := strings.Split(fileContent, "\n")
-
-	for index, entry := range entries {
-		fmt.Printf("%d - %s\n", index + 1, entry)
-	}
-
-	fmt.Println("Choose a operation:")
-	fmt.Println("1 - [C]reate a new entry")
-	fmt.Println("2 - [R]emove a entry")
-
-	var option string
-	fmt.Scan(&option)
-
-	if option == "1" || option == "C" || option == "c" {
-		entry := readLine()
-		entries = append(entries, entry)
-	} else if option == "2" || option == "R" || option == "r" {
-
-	} else {
+	for ;; {
+		printTasks(tasks)
+		option := getOption()
 		
+		if option == "1" || option == "C" || option == "c" {
+			addTask(&tasks)
+		} else if option == "2" || option == "R" || option == "r" {
+			removeTask(&tasks)
+		} else if option == "3" || option == "E" || option == "e" {
+			saveTasks(tasks, fileName)
+			return
+		} else {
+			fmt.Println("Invalid option")
+		}
 	}
 }
 
@@ -51,6 +43,71 @@ func getFileName() string {
 	fmt.Scan(&fileName)
 	
 	return fileName
+}
+
+func getTasksFromFile(fileName string) ([]string, error) {
+	fileContent, err := getFileContent(fileName)
+	if err != nil {
+		return nil, err
+	}
+	var tasks []string
+	if len(fileContent) > 0 {
+		tasks = strings.Split(fileContent, "\n")
+	}
+	return tasks, nil
+}
+
+func getFileContent(fileName string) (string, error) {
+	fileData, err := os.ReadFile(fileName)
+	if err != nil {
+		return "", err
+	}
+	return string(fileData), nil
+}
+
+func printTasks(tasks []string) {
+	if len(tasks) > 0 {	
+		fmt.Println("Tasks:")
+		for index, task := range tasks {
+			fmt.Printf("%d - %s\n", index + 1, task)
+		}
+	} else {
+		fmt.Println("No tasks available")
+	}
+}
+
+func getOption() string {
+	fmt.Println("\nChoose an option:")
+	fmt.Println("1 - [C]reate a new task")
+	fmt.Println("2 - [R]emove a task")
+	fmt.Println("3 - [E]xit and save")
+
+	var option string
+	fmt.Scanln(&option)
+
+	return option
+}
+
+func addTask(tasks *[]string) {
+	fmt.Println("Enter the new task")
+	task := readLine()
+	*tasks = append(*tasks, task)
+}
+
+func removeTask(tasks *[]string) {
+	fmt.Println("Select the number of the task to remove")
+	var taskIndex int32
+	fmt.Scanln(&taskIndex)
+	*tasks = append((*tasks)[:taskIndex-1], (*tasks)[taskIndex:]...)
+}
+
+func saveTasks(tasks []string, fileName string) {
+	fileData := []byte(strings.Join(tasks, "\n"))
+	err := os.WriteFile(fileName, fileData, 0777)
+	if err != nil {
+		fmt.Println("Error on writing file")
+		fmt.Println(err)
+	}
 }
 
 func readLine() string {
